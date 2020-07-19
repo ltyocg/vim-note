e4e677df285e388c347ae029868859df2abfe80f
John Liu <ltyocg@163.com>
2020-07-19 16:22:13 +0800

    删除配置文件

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ nvim_path="$(cd ~; pwd)/.config/nvim"
 base_path=$(cd `dirname $0`; pwd)
 for file in `ls`
 do
-    if [ $file != "README.md" -a $file != "link.sh" ]
+    if [ $file != "README.md" -a $file != "install.bat" ]
     then ln -s "${base_path}/${file}" "${nvim_path}/${file}"
     fi
 done
